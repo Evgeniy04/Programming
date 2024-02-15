062461a9c0572615b7c5440cd87f26b22cd046a2
AvraDev <evgenyiavra@gmail.com>
2024-02-15 12:05:06 +0700

    4.0.2

--- Programming/Model/CustomMethods.cs
+++ Programming/Model/CustomMethods.cs
@@ -7,17 +7,6 @@ static class CustomMethods
                                             typeof(Genre), typeof(Manufactures),
                                             typeof(Season), typeof(Weekday) };
 
-    static public bool TryGetEnumValue(Type itemType, string itemName, out object value)
-    {
-        if (itemType != null && TypeModel.Contains(itemType))
-        {
-            value = Enum.Parse(itemType, itemName);
-            return true;
-        }
-        value = null;
-        return false;
-    }
-
     static public bool TryGetEnumValue<T>(string itemName, out T value) where T : struct
     {
         if (Enum.TryParse<T>(itemName, true, out value))
