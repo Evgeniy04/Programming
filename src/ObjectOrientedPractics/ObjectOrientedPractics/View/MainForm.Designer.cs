5ab8495dcaca1c34c143a47f6e1b352461895422
Evgeniy04 <evgenyiavra@gmail.com>
2024-09-07 15:07:12 +0300

    Add ItemsTab and TabControl

--- src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            MainTabControl = new TabControl();
+            ItemsPage = new TabPage();
+            tabPage2 = new TabPage();
+            MainTabControl.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MainTabControl
+            // 
+            MainTabControl.Controls.Add(ItemsPage);
+            MainTabControl.Controls.Add(tabPage2);
+            MainTabControl.Dock = DockStyle.Fill;
+            MainTabControl.Location = new Point(0, 0);
+            MainTabControl.Name = "MainTabControl";
+            MainTabControl.SelectedIndex = 0;
+            MainTabControl.Size = new Size(800, 450);
+            MainTabControl.TabIndex = 0;
+            // 
+            // ItemsPage
+            // 
+            ItemsPage.Location = new Point(4, 29);
+            ItemsPage.Name = "ItemsPage";
+            ItemsPage.Padding = new Padding(3);
+            ItemsPage.Size = new Size(792, 417);
+            ItemsPage.TabIndex = 0;
+            ItemsPage.Text = "Items";
+            ItemsPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 417);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(MainTabControl);
+            Name = "MainForm";
+            Text = "Form1";
+            MainTabControl.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl MainTabControl;
+        private TabPage ItemsPage;
+        private TabPage tabPage2;
     }
 }
---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/ItemsTab.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ItemsPanel = new Panel();
+            ItemsListBox = new ListBox();
+            RemoveItemButton = new Button();
+            AddItemButton = new Button();
+            ItemsLabel = new Label();
+            SelectedItemPanel = new Panel();
+            DescriptionRichTextBox = new RichTextBox();
+            NameRichTextBox = new RichTextBox();
+            CostTextBox = new TextBox();
+            IdTextBox = new TextBox();
+            DescriptionLabel = new Label();
+            SelectedItemLabel = new Label();
+            NameLabel = new Label();
+            CostLabel = new Label();
+            IdLabel = new Label();
+            ItemsPanel.SuspendLayout();
+            SelectedItemPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ItemsPanel
+            // 
+            ItemsPanel.AutoSize = true;
+            ItemsPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ItemsPanel.Controls.Add(ItemsListBox);
+            ItemsPanel.Controls.Add(RemoveItemButton);
+            ItemsPanel.Controls.Add(AddItemButton);
+            ItemsPanel.Controls.Add(ItemsLabel);
+            ItemsPanel.Location = new Point(0, 0);
+            ItemsPanel.Name = "ItemsPanel";
+            ItemsPanel.Size = new Size(325, 485);
+            ItemsPanel.TabIndex = 2;
+            // 
+            // ItemsListBox
+            // 
+            ItemsListBox.FormattingEnabled = true;
+            ItemsListBox.Location = new Point(13, 36);
+            ItemsListBox.Name = "ItemsListBox";
+            ItemsListBox.Size = new Size(309, 404);
+            ItemsListBox.TabIndex = 4;
+            // 
+            // RemoveItemButton
+            // 
+            RemoveItemButton.Location = new Point(131, 447);
+            RemoveItemButton.Name = "RemoveItemButton";
+            RemoveItemButton.Size = new Size(112, 35);
+            RemoveItemButton.TabIndex = 5;
+            RemoveItemButton.Text = "Remove";
+            RemoveItemButton.UseVisualStyleBackColor = true;
+            // 
+            // AddItemButton
+            // 
+            AddItemButton.Location = new Point(13, 447);
+            AddItemButton.Name = "AddItemButton";
+            AddItemButton.Size = new Size(112, 35);
+            AddItemButton.TabIndex = 4;
+            AddItemButton.Text = "Add";
+            AddItemButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemsLabel
+            // 
+            ItemsLabel.AutoSize = true;
+            ItemsLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            ItemsLabel.Location = new Point(13, 13);
+            ItemsLabel.Name = "ItemsLabel";
+            ItemsLabel.Size = new Size(49, 20);
+            ItemsLabel.TabIndex = 0;
+            ItemsLabel.Text = "Items";
+            // 
+            // SelectedItemPanel
+            // 
+            SelectedItemPanel.AutoSize = true;
+            SelectedItemPanel.BackColor = SystemColors.ControlLightLight;
+            SelectedItemPanel.Controls.Add(DescriptionRichTextBox);
+            SelectedItemPanel.Controls.Add(NameRichTextBox);
+            SelectedItemPanel.Controls.Add(CostTextBox);
+            SelectedItemPanel.Controls.Add(IdTextBox);
+            SelectedItemPanel.Controls.Add(DescriptionLabel);
+            SelectedItemPanel.Controls.Add(SelectedItemLabel);
+            SelectedItemPanel.Controls.Add(NameLabel);
+            SelectedItemPanel.Controls.Add(CostLabel);
+            SelectedItemPanel.Controls.Add(IdLabel);
+            SelectedItemPanel.Location = new Point(331, 3);
+            SelectedItemPanel.Name = "SelectedItemPanel";
+            SelectedItemPanel.Size = new Size(484, 491);
+            SelectedItemPanel.TabIndex = 3;
+            // 
+            // DescriptionRichTextBox
+            // 
+            DescriptionRichTextBox.Location = new Point(13, 257);
+            DescriptionRichTextBox.Name = "DescriptionRichTextBox";
+            DescriptionRichTextBox.Size = new Size(468, 145);
+            DescriptionRichTextBox.TabIndex = 13;
+            DescriptionRichTextBox.Text = "";
+            // 
+            // NameRichTextBox
+            // 
+            NameRichTextBox.Location = new Point(13, 140);
+            NameRichTextBox.Name = "NameRichTextBox";
+            NameRichTextBox.Size = new Size(468, 82);
+            NameRichTextBox.TabIndex = 12;
+            NameRichTextBox.Text = "";
+            // 
+            // CostTextBox
+            // 
+            CostTextBox.Location = new Point(84, 81);
+            CostTextBox.Name = "CostTextBox";
+            CostTextBox.Size = new Size(125, 27);
+            CostTextBox.TabIndex = 11;
+            // 
+            // IdTextBox
+            // 
+            IdTextBox.Location = new Point(84, 48);
+            IdTextBox.Name = "IdTextBox";
+            IdTextBox.Size = new Size(125, 27);
+            IdTextBox.TabIndex = 10;
+            // 
+            // DescriptionLabel
+            // 
+            DescriptionLabel.AutoSize = true;
+            DescriptionLabel.Location = new Point(13, 234);
+            DescriptionLabel.Name = "DescriptionLabel";
+            DescriptionLabel.Size = new Size(88, 20);
+            DescriptionLabel.TabIndex = 9;
+            DescriptionLabel.Text = "Description:";
+            // 
+            // SelectedItemLabel
+            // 
+            SelectedItemLabel.AutoSize = true;
+            SelectedItemLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            SelectedItemLabel.Location = new Point(13, 13);
+            SelectedItemLabel.Name = "SelectedItemLabel";
+            SelectedItemLabel.Size = new Size(104, 20);
+            SelectedItemLabel.TabIndex = 1;
+            SelectedItemLabel.Text = "Selected Item";
+            // 
+            // NameLabel
+            // 
+            NameLabel.AutoSize = true;
+            NameLabel.Location = new Point(13, 117);
+            NameLabel.Name = "NameLabel";
+            NameLabel.Size = new Size(52, 20);
+            NameLabel.TabIndex = 8;
+            NameLabel.Text = "Name:";
+            // 
+            // CostLabel
+            // 
+            CostLabel.AutoSize = true;
+            CostLabel.Location = new Point(13, 84);
+            CostLabel.Name = "CostLabel";
+            CostLabel.Size = new Size(41, 20);
+            CostLabel.TabIndex = 7;
+            CostLabel.Text = "Cost:";
+            // 
+            // IdLabel
+            // 
+            IdLabel.AutoSize = true;
+            IdLabel.Location = new Point(13, 51);
+            IdLabel.Name = "IdLabel";
+            IdLabel.Size = new Size(27, 20);
+            IdLabel.TabIndex = 6;
+            IdLabel.Text = "ID:";
+            // 
+            // ItemsTab
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(SelectedItemPanel);
+            Controls.Add(ItemsPanel);
+            Name = "ItemsTab";
+            Size = new Size(818, 497);
+            ItemsPanel.ResumeLayout(false);
+            ItemsPanel.PerformLayout();
+            SelectedItemPanel.ResumeLayout(false);
+            SelectedItemPanel.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel ItemsPanel;
+        private Label ItemsLabel;
+        private Panel SelectedItemPanel;
+        private Button AddItemButton;
+        private Label SelectedItemLabel;
+        private Button RemoveItemButton;
+        private ListBox ItemsListBox;
+        private Label DescriptionLabel;
+        private Label NameLabel;
+        private Label CostLabel;
+        private Label IdLabel;
+        private TextBox IdTextBox;
+        private RichTextBox DescriptionRichTextBox;
+        private RichTextBox NameRichTextBox;
+        private TextBox CostTextBox;
+    }
+}
