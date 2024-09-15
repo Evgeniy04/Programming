e984dbbff1611bb43099e5933fbbef66e645900f
Evgeniy04 <evgenyiavra@gmail.com>
2024-09-15 10:43:08 +0300

    Created PriorityOrdersTab

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/Tabs/PriorityOrdersTab.cs
@@ -0,0 +1,49 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Tabs
+{
+    public partial class PriorityOrdersTab : UserControl
+    {
+        /// <summary>
+        /// Поле приоритетного заказа.
+        /// </summary>
+        PriorityOrder _priorityOrder;
+
+        /// <summary>
+        /// Получает и задаёт значение свойства приоритетного заказа.
+        /// </summary>
+        PriorityOrder PriorityOrder
+        {
+            get
+            {
+                return _priorityOrder;
+            }
+            set
+            {
+                _priorityOrder = value;
+            }
+        }
+
+        public PriorityOrdersTab()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Отключает ввод данных в текстовое поле при нажатии клавиши.
+        /// </summary>
+        private void DisableTextBox(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
