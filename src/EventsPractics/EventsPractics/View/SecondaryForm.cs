9168f0b1b33e5aa495fcbce6df9aff5e3dfdd1eb
Evgeniy04 <evgenyiavra@gmail.com>
2024-09-22 21:08:00 +0300

    EventsPractics application developed

---
+++ src/EventsPractics/EventsPractics/View/SecondaryForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventsPractics.View
+{
+    public partial class SecondaryForm : Form
+    {
+        public SecondaryForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
