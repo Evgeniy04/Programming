0debc51f981f6d8df6bcffab4070c49ccc6a052d
Evgeniy04 <evgenyiavra@gmail.com>
2025-02-19 17:38:19 +0300

    Added comments for State enum.

--- src/Contacts/Contacts/Model/Enums/State.cs
+++ src/Contacts/Contacts/Model/Enums/State.cs
@@ -1,9 +1,21 @@
 ﻿namespace View.Model.Enums
 {
+    /// <summary>
+    /// Состояния приложения.
+    /// </summary>
     public enum State
     {
+        /// <summary>
+        /// Контакты просматриваются.
+        /// </summary>
         Reading,
+        /// <summary>
+        /// Добавляется новый контакт.
+        /// </summary>
         Adding,
+        /// <summary>
+        /// Редактируется имеющийся контакт.
+        /// </summary>
         Editing,
     }
 }
